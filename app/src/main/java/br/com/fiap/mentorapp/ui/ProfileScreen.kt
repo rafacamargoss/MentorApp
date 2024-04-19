68a6a5614b5bed70828e137d13375335408f3d07
JoaoLiraDev <lirajoao414@gmail.com>
2024-04-18 21:16:22 -0300

    style: ajustando layout tela de perfil do usuário

--- app/src/main/java/br/com/fiap/mentorapp/ui/ProfileScreen.kt
+++ app/src/main/java/br/com/fiap/mentorapp/ui/ProfileScreen.kt
@@ -3,6 +3,7 @@ package br.com.fiap.mentorapp.ui
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,9 +17,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -29,7 +33,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,6 +49,7 @@ import br.com.fiap.mentorapp.R
 import br.com.fiap.mentorapp.navigateTo
 import br.com.fiap.mentorapp.ui.components.OutlineSelectField
 import br.com.fiap.mentorapp.ui.components.SelectOptionsType
+import br.com.fiap.mentorapp.ui.theme.OpenSansBold
 import br.com.fiap.mentorapp.ui.theme.OpenSansRegular
 
 enum class Gender {
@@ -342,7 +349,23 @@ fun ProfileForm(
                 .padding(16.dp),
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = "Logout", modifier = Modifier.clickable { onLogout.invoke() })
+            OutlinedButton(
+                onClick = { onLogout.invoke() },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                border = BorderStroke(width = 0.5.dp, color = colorResource(id = R.color.primary_100))
+            ) {
+                Text(
+                    text = "Logout",
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = OpenSansBold,
+                    color = colorResource(id = R.color.primary_100),
+                    fontSize = 13.sp
+                )
+            }
+            ClickableText(text = AnnotatedString(text = "Logout"), onClick = { onLogout.invoke() })
+
         }
 
     }
